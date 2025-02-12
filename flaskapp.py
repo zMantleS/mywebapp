8bee2e9d43be7267af7414bb7cb0fd82a3b19b3a
zmantles <mantleseb@gmail.com>
2025-02-12 14:43:01 +0000

    Ok

--- flaskapp.py
+++ flaskapp.py
@@ -32,7 +32,7 @@ def insert():
             except Exception as e:
                 return render_template("insert.html", status="Bad")
     else:
-        # helloo
+        # hellooo
         return render_template("insert.html")
 
 
